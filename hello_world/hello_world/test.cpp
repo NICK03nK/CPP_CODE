e4f1e1f3b75556863c0707dc0af3b96976a533da
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-09-15 21:54:22 +0800

    hello_world

---
+++ hello_world/hello_world/test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+
+	//int a;
+	//cout << a << endl;   // 不初始化变量a会编译报错！
+
+	return 0;
+}
